d6f4ef8e6a54cd0bbf7aeb13dde2c8621cc0f06c
M <M>
2025-01-25 12:15:14 +0200

    ++

--- src/main/java/school/mjc/stage0/module3/task1/BrokenApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/BrokenApp.java
@@ -1,10 +1,7 @@
 package school.mjc.stage0.module3.task1;
 
 public class BrokenApp {
-    BrokenApp() {
-        System.out.println("fixMe");
-    }
     public static void main(String[] args) {
-        new BrokenApp();
+        System.out.println("fixMe");
     }
 }
