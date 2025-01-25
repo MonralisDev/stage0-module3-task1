ce772a3e7e02f4fbfb367d191de02b9219c94656
M <M>
2025-01-25 12:16:11 +0200

    ++

--- src/main/java/school/mjc/stage0/module3/task1/PrintSentenceApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/PrintSentenceApp.java
@@ -1,10 +1,7 @@
 package school.mjc.stage0.module3.task1;
 
 public class PrintSentenceApp {
-    PrintSentenceApp() {
-        System.out.println("This is my first Java program");
-    }
     public static void main(String[] args) {
-        new PrintSentenceApp();
+        System.out.println("This is my first Java program");
     }
 }
